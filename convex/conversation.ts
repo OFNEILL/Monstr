2d352707fdf5748b28a39eedf297448bdad64f2d
OFNEILL <oneillj06@outlook.com>
2024-08-30 08:06:01 +0100

    open/close

--- convex/conversation.ts
+++ convex/conversation.ts
@@ -1,9 +1,25 @@
-import { query, mutation } from "_generated/server";
+import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
 
-export const manage = query({
-  args: { id: v.string(), open: v.boolean() },
-  handler: async (ctx, { id, open }) => {
-    await ctx.db.insert("conversations", { id, open: true });
+export const openConversation = query({
+  args: {},
+  handler: async (ctx: any) => {
+    //create convo
+    console.log("opening conversation");
+    const conversation = await ctx.db.insert("conversations");
+
+    //get convoId
+    console.log("getting id");
+    const conversationId = await ctx.get(conversation)._id;
+    console.log(conversationId);
+  },
+});
+
+export const closeConversation = query({
+  args: { id: v.string() },
+  handler: async (ctx: any, id: number) => {
+    //delete convo from table
+    await ctx.db.delete("conversation", { _id: id });
+    console.log("deleted");
   },
 });
