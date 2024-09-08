ae4dabd5ccb88dacef41a54ad1efb2633bca7377
Adrian Tobi <talktotobi.a@gmail.com>
2024-09-08 19:52:38 +0100

    fix auth redirect

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { SignIn, useAuth } from "@clerk/nextjs";
-import { Authenticated, ConvexReactClient, Unauthenticated } from "convex/react";
+import {
+  Authenticated,
+  ConvexReactClient,
+  Unauthenticated,
+} from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ReactNode } from "react";
 
@@ -10,19 +14,25 @@ const convex = new ConvexReactClient(
   "https://festive-sturgeon-235.convex.cloud",
 );
 
-export function ConvexClientProvider({ children, authRequired }: { children: ReactNode, authRequired?: boolean}) {
+export function ConvexClientProvider({
+  children,
+  authRequired,
+}: {
+  children: ReactNode;
+  authRequired?: boolean;
+}) {
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {authRequired ? <>{children}</> : 
+      {!authRequired ? (
+        <>{children}</>
+      ) : (
         <>
-        <Authenticated>
-          {children}
-          </Authenticated>
-      <Unauthenticated>
-        <SignIn />
-      </Unauthenticated>
-      </>
-    }
+          <Authenticated>{children}</Authenticated>
+          <Unauthenticated>
+            <SignIn />
+          </Unauthenticated>
+        </>
+      )}
     </ConvexProviderWithClerk>
   );
 }
